5c316fad76cabf685189c785f177672796cd40f2
Yomna Abdelmegeed <yomna.tota4@gmail.com>
2025-07-04 00:24:01 +0300

    convert to FutureBuilder.

--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -1,47 +1,44 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app/models/article_model.dart';
 import 'package:news_app/services/news_services.dart';
 import 'package:news_app/widgets/news_list_view.dart';
 
-class NewsListViewBuilder extends StatefulWidget {
+class NewsListViewBuilder extends StatelessWidget {
   final String? cat;
 
   const NewsListViewBuilder({super.key, this.cat});
 
   @override
-  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
-}
-
-class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
-  List<ArticleModel> articles = [];
-
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    getGeneralNews();
-  }
-
-  Future<void> getGeneralNews() async {
-    articles = await NewsServices(Dio()).gettNews(category:  widget.cat);
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? const SliverFillRemaining(
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          )
-        : articles.isNotEmpty
-            ? NewsListView(articles: articles)
-            : const SliverToBoxAdapter(
-                child: Text('opps there was an error'),
-              );
+    return FutureBuilder(
+      future: NewsServices(Dio()).gettNews(category: cat),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return NewsListView(articles: snapshot.data!);
+        } else if (snapshot.hasError) {
+          return const SliverToBoxAdapter(
+              child: Text('opps there was an error'));
+        } else {
+          return const SliverFillRemaining(
+              child: Center(child: CircularProgressIndicator()));
+        }
+      },
+    );
   }
 }
+
+
+
+
+
+    //  isLoading
+    //     ? const SliverFillRemaining(
+    //         child: Center(
+    //           child: CircularProgressIndicator(),
+    //         ),
+    //       )
+    //     : articles.isNotEmpty
+    //         ? NewsListView(articles: articles)
+    //         : const SliverToBoxAdapter(
+    //             child: Text('opps there was an error'),
+    //           );
